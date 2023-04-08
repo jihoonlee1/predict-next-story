c01c5ad0cf6a93d7e3c0223804507837f7af50ac
Ji Hoon Lee <jihoon.lee@morningstar.com>
2023-04-08 08:38:51 -0400

    Changed database schema.

--- database.py
+++ database.py
@@ -5,32 +5,33 @@ import sqlite3
 dbpath = "database.sqlite"
 statements = [
 """
-CREATE TABLE IF NOT EXISTS companies(
+CREATE TABLE companies(
 	id   INTEGER NOT NULL PRIMARY KEY,
 	name TEXT    NOT NULL
-);
+)
 """,
 """
-CREATE TABLE IF NOT EXISTS incidents(
+CREATE TABLE incidents(
 	id         INTEGER NOT NULL PRIMARY KEY,
 	content    TEXT    NOT NULL,
+	is_chatgpt INTEGER NOT NULL,
 	company_id INTEGER NOT NULL
 )
 """,
 """
-CREATE TABLE IF NOT EXISTS events(
-	id          INTEGER NOT NULL PRIMARY KEY,
-	company_id  INTEGER NOT NULL,
-	is_relevant INTEGER NOT NULL
-);
+CREATE TABLE events(
+	id             INTEGER NOT NULL PRIMARY KEY,
+	company_id     INTEGER NOT NULL,
+	is_relevant    INTEGER NOT NULL
+)
 """,
 """
-CREATE TABLE IF NOT EXISTS event_incident(
-	event_id    INTEGER NOT NULL,
-	incident_id INTEGER NOT NULL,
-	story_order INTEGER NOT NULL,
+CREATE TABLE event_incident(
+	event_id       INTEGER NOT NULL REFERENCES events(id),
+	incident_id    INTEGER NOT NULL REFERENCES incidents(id),
+	incident_order INTEGER NOT NULL,
 	PRIMARY KEY(event_id, incident_id)
-);
+)
 """
 ]
 
