6c6cd5091f196da0cd966ce9abca74639867174f
Ji Hoon Lee <jihoon.lee@morningstar.com>
2023-04-11 00:20:53 -0400

    Added training loop.

--- train_model.py
+++ train_model.py
@@ -1,15 +1,17 @@
-from transformers import AutoTokenizer, BertForNextSentencePrediction
+from transformers import AutoTokenizer, BertForNextSentencePrediction, logging
 import database
 import random
 import re
 import torch
 
 
-learning_rate = 0.003
+logging.set_verbosity_error()
+device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+learning_rate = 1e-3
 epochs = 3
 batch_size = 8
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-model = BertForNextSentencePrediction.from_pretrained("bert-base-uncased")
+model = BertForNextSentencePrediction.from_pretrained("bert-base-uncased").to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 
@@ -17,6 +19,11 @@ class IncidentDataset(torch.utils.data.Dataset):
 	def __init__(self, encodings):
 		self.encodings = encodings
 
+	def __len__(self):
+		return self.encodings.input_ids.shape[0]
+
+	def __getitem__(self, idx):
+		return {key: tensor[idx] for key, tensor in self.encodings.items()}
 
 
 def positive_data(cur, sentence0, sentence1, labels):
@@ -49,6 +56,7 @@ def negative_data(cur, sentence0, sentence1, labels, incidents_all, num_incident
 	while iteration < num_positives:
 		random_number = random.randint(0, num_incidents_all-1)
 		target_incident_id, target_content = incidents_all[random_number]
+		target_content = re.sub(r"\n+", " ", target_content)
 		cur.execute("SELECT event_id FROM event_incident WHERE incident_id = ?", (target_incident_id, ))
 		event_id, = cur.fetchone()
 		cur.execute("""
@@ -62,6 +70,7 @@ def negative_data(cur, sentence0, sentence1, labels, incidents_all, num_incident
 		num_unrelated_incidents = len(unrelated_incidents)
 		random_number = random.randint(0, num_unrelated_incidents-1)
 		unrelated_content, = unrelated_incidents[random_number]
+		unrelated_content = re.sub(r"\n+", " ", unrelated_content)
 		sentence0.append(target_content)
 		sentence1.append(unrelated_content)
 		labels.append(1)
@@ -69,15 +78,39 @@ def negative_data(cur, sentence0, sentence1, labels, incidents_all, num_incident
 	return (sentence0, sentence1, labels)
 
 
+def train_loop(dataloader, model, optimizer):
+	for batch in dataloader:
+		input_ids = batch["input_ids"].to(device)
+		token_type_ids = batch["token_type_ids"].to(device)
+		attention_mask = batch["attention_mask"].to(device)
+		labels = batch["labels"].to(device)
+		outputs = model(input_ids.to(device), token_type_ids=token_type_ids.to(device), attention_mask=attention_mask.to(device), labels=labels.to(device))
+		loss = outputs.loss
+		optimizer.zero_grad()
+		loss.backward()
+		optimizer.step()
+		print(f"loss: {loss.item():>7f}")
+
+
 def main():
-	sentence0, sentence1, labels = [], [], []
 	with database.connect() as con:
 		cur = con.cursor()
 		cur.execute("SELECT id, content FROM incidents")
 		incidents_all = cur.fetchall()
 		num_incidents_all = len(incidents_all)
+		offset_start = 0
+		offset_end = 3
+		sentence0, sentence1, labels = [], [], []
 		sentence0, sentence1, labels = positive_data(cur, sentence0, sentence1, labels)
 		sentence0, sentence1, labels = negative_data(cur, sentence0, sentence1, labels, incidents_all, num_incidents_all)
+		inputs = tokenizer(sentence0, sentence1, return_tensors="pt", max_length=512, truncation=True, padding="max_length")
+		inputs["labels"] = torch.LongTensor([labels]).T
+		dataset = IncidentDataset(inputs)
+		dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+		for epoch in range(epochs):
+			print(f"Epoch {epoch + 1}")
+			train_loop(dataloader, model, optimizer)
+		model.save(model, "model.pth")
 
 
 if __name__ == "__main__":
