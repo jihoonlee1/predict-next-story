b5c6cce2d9951840291b33a6593cbbcb200c488d
Ji Hoon Lee <jihoon.lee@morningstar.com>
2023-04-10 23:48:50 -0400

    Check to make sure 30 incidents per company.

--- clean_data.py
+++ clean_data.py
@@ -4,10 +4,11 @@ import database
 def main():
 	with database.connect() as con:
 		cur = con.cursor()
-		cur.execute("SELECT id, name FROM companies")
-		all_companies = cur.fetchall()
-		for company_id, company_name in all_companies:
-			cur.execute("")
+		cur.execute("SELECT count(*), company_id FROM incidents GROUP BY company_id")
+		for count, company_id in cur.fetchall():
+			if count != 30:
+				print(company_id)
+
 
 if __name__ == "__main__":
 	main()
