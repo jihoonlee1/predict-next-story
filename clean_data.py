6a0389f43a5b54d10179d4aa80c81f0c1e071ed4
Ji Hoon Lee <jihoon.lee@morningstar.com>
2023-04-10 23:47:05 -0400

    Added clean data script.

--- clean_data.py
+++ clean_data.py
@@ -6,12 +6,8 @@ def main():
 		cur = con.cursor()
 		cur.execute("SELECT id, name FROM companies")
 		all_companies = cur.fetchall()
-		num_companies = len(all_companies)
 		for company_id, company_name in all_companies:
-			cur.execute("SELECT 1 FROM incidents WHERE company_id = ?", (company_id, ))
-			if cur.fetchone() is None:
-				print(company_id, company_name)
-
+			cur.execute("")
 
 if __name__ == "__main__":
 	main()
